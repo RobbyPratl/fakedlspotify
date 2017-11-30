71254e22fafd28ee2d6cadfba9209f2fa56ea1c4
RobbyPratl <34143233+RobbyPratl@users.noreply.github.com>
2017-11-30 16:54:13 -0600

    Add files via upload

---
+++ jsspotify.js
@@ -0,0 +1,12 @@
+
+
+(function() {
+    var link = document.querySelector("link[rel*='icon']") || document.createElement('link');
+    link.type = 'image/x-icon';
+    link.rel = 'shortcut icon';
+    link.href = 'https://www.duolingo.com/images/favicon.ico?v=3';
+    document.getElementsByTagName('head')[0].appendChild(link);
+})();
+setInterval(function(){ 
+    document.title = 'Duolingo';   
+}, 5000);
