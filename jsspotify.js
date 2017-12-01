d35fe2eb2e260011245845b3da7bbf4759f1f6f9
RobbyPratl <34143233+RobbyPratl@users.noreply.github.com>
2017-11-30 18:17:05 -0600

    Update jsspotify.js

--- jsspotify.js
+++ jsspotify.js
@@ -1,5 +1,5 @@
 // made by robert pratl @robby.pratl on ig @robbypratl on twitter
-
+// run this in the chrome developer console to make any tab look like duolingo's tab
 (function() {
     var link = document.querySelector("link[rel*='icon']") || document.createElement('link');
     link.type = 'image/x-icon';
